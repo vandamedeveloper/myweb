deca0b876fb33018de519266b4bf57f6264da554
Vandi Hrustic <>
2022-04-07 14:57:59 +0200

    added router library

--- src/components/HEADER/Header.js
+++ src/components/HEADER/Header.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 function Header() {
   return (
     <header>
-        <h1>I am the header</h1>
+      <div className="container">
+        <nav className="nav">
+          <div className="menu-toggle">
+            <i className="fas fa-times"></i>
+            <i className="fas fa-bars"></i>
+          </div>
+          <img src = "https://placeholderlogo.com/img/placeholder-logo-1.png" alt = "vandame hrustic"/>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
